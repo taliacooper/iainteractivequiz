0d47208a3a9e029f2eebf044294712bb1da528b2
taliacooper <44005833+taliacooper@users.noreply.github.com>
2019-03-24 14:56:14 -0700

    Update Runner.java

--- src/Runner.java
+++ src/Runner.java
@@ -3,14 +3,22 @@ public class Runner {
     public static void main(String[] args){
         QuestionGroup p = new QuestionGroup();
         Scanner sc = new Scanner(System.in);
+        int score = 0;
+        boolean lastQuestionCorrect = false;
         for(int i = 0; i< p.gameLength; i++){
-            Question z = p.determineQuestion();
+
+            Question z = p.determineQuestion(lastQuestionCorrect);
             System.out.println(z.text);
             z.answeredCorrectly = (z.answer == sc.nextInt());
-
-            //for testing!!!
+            lastQuestionCorrect = z.answeredCorrectly;
             System.out.println(z.answeredCorrectly);
+            if(z.answeredCorrectly){
+                score++;
+
+            }
         }
+
+        System.out.println("your score was" + score + "/" + p.gameLength);
         //p.computeResult();
     }
 }
